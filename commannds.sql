f1c4ad18be2c5163f460ada6d80371d2db790e11
Yaroslav <romashkoyp@gmail.com>
2024-06-30 12:26:45 +0300

    13.2

---
+++ commannds.sql
@@ -0,0 +1,11 @@
+CREATE TABLE blogs (
+  id        SERIAL PRIMARY KEY,     --unique, incrementing id
+  author    TEXT,                   --string
+  url       TEXT NOT NULL,          --string that cannot be empty
+  title     TEXT NOT NULL,          --string that cannot be empty
+  likes     INTEGER DEFAULT 0       --integer with default value zero
+);
+
+INSERT INTO blogs (author, url, title) values ('Martin', 'www.martin.com', 'Live of Martin');
+INSERT INTO blogs (author, url, title, likes) values ('Luter', 'www.luter.com', 'Luter from UK', '12');
+SELECT * FROM blogs;
