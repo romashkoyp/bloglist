e48c25401af2b9b906a9c6f6aad84c5e201fdde3
Yaroslav <romashkoyp@gmail.com>
2024-07-01 17:17:51 +0300

    13.4 GET

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,63 @@
 require('dotenv').config()
-const { Sequelize, QueryTypes } = require('sequelize')
+const { Sequelize, QueryTypes, DataTypes, Model } = require('sequelize')
+const express = require('express')
+const app = express()
 
 const sequelize = new Sequelize(process.env.DATABASE_URL);
 
-const main = async () => {
+const dbConnect = async () => {
   try {
     await sequelize.authenticate()
     console.log('Connected to Postgres.')
-    const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
-    blogs.forEach(blog => {
-      console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`)
-    })
+    //const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
+    //blogs.forEach(blog => {
+    //  console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`)
+    //})
     //console.log(blogs)
-    sequelize.close()
+    //sequelize.close()
   } catch (error) {
     console.error('Unable to connect to the Postgres', error)
   }
 }
 
-main()
+class Blog extends Model {}
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.TEXT
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  },
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
+})
+
+app.get('/api/blogs', async (req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs)
+})
+
+app.use(express.json())
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
