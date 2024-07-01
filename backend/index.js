5c376f18ff499d9ce59afb5224a6d40de1f9973b
Yaroslav <romashkoyp@gmail.com>
2024-07-01 16:31:36 +0300

    13.3

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 require('dotenv').config()
-const { Sequelize } = require('sequelize')
+const { Sequelize, QueryTypes } = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DATABASE_URL)
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 const main = async () => {
   try {
     await sequelize.authenticate()
     console.log('Connected to Postgres.')
+    const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
+    blogs.forEach(blog => {
+      console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`)
+    })
+    //console.log(blogs)
     sequelize.close()
   } catch (error) {
     console.error('Unable to connect to the Postgres', error)
