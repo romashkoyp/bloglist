32f5d615b7e53f9e33f150230c6abd2b2d365799
Yaroslav <romashkoyp@gmail.com>
2024-07-02 12:57:27 +0300

    13.7

--- backend/controllers/blogs.js
+++ backend/controllers/blogs.js
@@ -1,44 +1,39 @@
 const router = require('express').Router()
-
 const { Blog } = require('../models')
 
 const blogFinder = async (req, res, next) => {
   req.blog = await Blog.findByPk(req.params.id)
+  if (!req.blog) {
+    throw new Error('Blog not found') // Throw error for middleware to catch
+  }
   next()
-} 
+}
+
+router.post('/', async (req, res) => {
+  const blog = await Blog.create(req.body)
+  if (blog) {
+    console.log(blog.toJSON())
+    res.status(201).json(blog)
+  } else {
+    res.status(400).end()
+  }
+})
 
 router.get('/', async (req, res) => {
   const blogs = await Blog.findAll()
   res.json(blogs)
 })
 
-router.post('/', async (req, res) => {
-  try {
-    const blog = await Blog.create(req.body)
-    return res.json(blog)
-  } catch (error) {
-    return res.status(400).json({ error })
-  }
-})
-
 router.delete('/:id', blogFinder, async (req, res) => {
-  if (!req.blog) {
-    return res.status(404).json({ error: 'Blog not found' })
-  } else {
-    await req.blog.destroy()
-  }
+  await req.blog.destroy()
   console.log('Blog was deleted')
-  return res.status(204).end()
+  res.status(204).end()
 })
 
 router.put('/:id', blogFinder, async (req, res) => {
-  if (!req.blog) {
-    return res.status(404).json({ error: 'Blog not found' })
-  } else {
-    req.blog.likes = req.body.likes
-    await req.blog.save()
-    res.json(req.blog)
-  }
+  req.blog.likes = req.body.likes
+  await req.blog.save()
+  res.json(req.blog)
   console.log('Likes were updated')
 })
 
