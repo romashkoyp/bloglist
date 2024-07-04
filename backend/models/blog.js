3ebeb450ee1f0ba101b271959f48a33a44d5e319
Yaroslav <romashkoyp@gmail.com>
2024-07-04 12:09:09 +0300

    13.18

--- backend/models/blog.js
+++ backend/models/blog.js
@@ -23,6 +23,14 @@ Blog.init({
     type: DataTypes.INTEGER,
     defaultValue: 0
   },
+  year: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 1991,
+      max: new Date().getFullYear()
+    }
+  }
 }, {
   sequelize,
   underscored: true,
