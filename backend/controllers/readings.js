a309cf2dcf11c74c0428374292fec74c995d192b
Yaroslav <romashkoyp@gmail.com>
2024-07-05 16:53:34 +0300

    13.22

--- backend/controllers/readings.js
+++ backend/controllers/readings.js
@@ -1,5 +1,28 @@
 const router = require('express').Router()
-const { Reading } = require('../models')
+const { Reading, User } = require('../models')
+const { tokenExtractor } = require('../util/middleware')
+
+const readingFinder = async (req, res, next) => {
+  req.reading = await Reading.findByPk(req.params.id)
+  if (!req.reading) {
+    throw new Error('Not found')
+  }
+  next()
+}
+
+router.put('/:id', tokenExtractor, readingFinder, async (req, res) => {
+  const user = await User.findByPk(req.decodedToken.id)
+  //console.log(user.id)
+  //console.log(req.reading)
+  if (req.reading && req.reading.userId === user.id) {
+    req.reading.read = req.body.read
+    await req.reading.save()
+    res.json(req.reading)
+    console.log('Read status updated')
+  } else {
+    throw new Error ('Permission denied, you can mark blog as read/unread only from your own reading list')
+  }
+})
 
 router.post('/', async (req, res) => {
   //console.log(req.body)
